a02e78feeb0a8f4b3927ffd3fac0248384c6dcf3
Michal Sinka <michal.sinka@gmail.com>
2025-01-10 10:14:47 +0100

    Updated Wwise project metadata

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,44 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_BOSS_LINE_PHASE1 = 2924361411U;
+        static const AkUniqueID PLAY_BOSS_LINE_PHASE3 = 2924361409U;
+        static const AkUniqueID TEST_SOUND = 1936185617U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID BOSS_BUS = 2633636108U;
+        static const AkUniqueID DIALOGUE_BUS = 3714968413U;
+        static const AkUniqueID ENVIRONMENTAL_BUS = 3600197733U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID PLAYER_BUS = 174537428U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
